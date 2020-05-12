e63eca6b244d9d6025aeb011e026945052d80277
Tupal-cell <55320896+Tupal-cell@users.noreply.github.com>
2020-05-12 17:19:12 +0200

    var

--- Snake/script.js
+++ Snake/script.js
@@ -1,19 +1,19 @@
 window.onload = function()
 {
 
-    let canvasWidth = 900;
-    let canvasHeight = 600;
-    let blockSize = 30;
-    let ctx;
-    let delay = 100;
-    let snakee;
+    var canvasWidth = 900;
+    var canvasHeight = 600;
+    var blockSize = 30;
+    var ctx;
+    var delay = 100;
+    var snakee;
 
     init();
 
     function init()
     {
 
-        let  canvas = document.createElement('canvas');
+        var canvas = document.createElement('canvas');
         canvas.width = canvasWidth;
         canvas.height = canvasHeight;
         canvas.style.border = "1px solid";
@@ -21,7 +21,7 @@ window.onload = function()
         ctx = canvas.getContext('2d');
         snakee = new Snake ([[6,4],[5,4],[4,4]]);
         refreshCanvas();
-    };
+    }
 
     function refreshCanvas()
     {     
@@ -29,14 +29,14 @@ window.onload = function()
         snakee.advance();
         snakee.draw();
         setTimeout(refreshCanvas, delay);
-    };
+    }
 
     function drawBlock(ctx, position)
     {
-        let x = position[0] * blockSize;
-        let y = position[1] * blockSize;
-        ctx.fillRect(x, y, blockSize, blockSize);
-    };
+        var x = position[0] * blockSize;
+        var y = position[1] * blockSize;
+        ctx.fillRect(x ,y , blockSize, blockSize);
+    }
 
     function Snake(body)
     {
@@ -45,7 +45,7 @@ window.onload = function()
         {
             ctx.save();
             ctx.fillStyle = "#ff0000";
-            for(var i = 0; i < this.body.lenght; i++);
+            for(var i = 0; i < this.body.length; i++);
             {
                 drawBlock(ctx, this.body[i]);
             }
