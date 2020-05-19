0f2091830c8ba4a30d9bfa087af55036734f5669
Tupal-cell <55320896+Tupal-cell@users.noreply.github.com>
2020-05-19 17:38:52 +0200

    optimal + keyfunction

--- Snake/script.js
+++ Snake/script.js
@@ -18,7 +18,7 @@ window.onload = function()
         ctx = canvas.getContext('2d');
         snakee =new Snake ([[6,4], [5,4], [4,4]], "right");
         refreshCanvas();
-    }
+    };
 
     function refreshCanvas ()
     {
@@ -26,35 +26,92 @@ window.onload = function()
         snakee.advance();
         snakee.draw();
         setTimeout(refreshCanvas,delay);
-    }
+    };
 
     function drawBlock(ctx, position,)
     {
         var x = position[0] * blockSize;
         var y = position[1] * blockSize;
         ctx.fillRect(x,y,blockSize, blockSize);
-    }
+    };
+
     function Snake(body, direction) 
     {
         this.body = body;
         this.direction = direction;
+
         this.draw = function () 
         {
             ctx.save();
             ctx.fillStyle ="#ff0000";
             for(var i=0; i<this.body.length; i++)
-            {
-                drawBlock(ctx, this.body[i]);
-            }
+                {
+                    drawBlock(ctx, this.body[i]);
+                }
             ctx.restore();
         };
+
         this.advance = function()
         {
             var nextPosition = this.body[0].slice();
-            nextPosition[0] += 1;
+            switch(this.direction)
+            {
+                case "left":
+                    nextPosition[0] -= 1;
+                    break;
+                case "right":
+                    nextPosition[0] += 1;
+                    break;
+                case "down":
+                    nextPosition[1] += 1;
+                    break;
+                case "up":
+                    nextPosition[1] -= 1;
+                    break;
+            }
+
             this.body.unshift(nextPosition);
             this.body.pop();
-
         };
-    }
+
+        this.setDirection(newDirection)
+        {
+            var allowedDirections;
+
+            switch(this.direction)
+            {
+                case "left":
+                case "right":
+                    allowedDirections = ["up", "down"];
+                    break;
+                case "down":
+                case "up":
+                    allowedDirections = ["left","right"];
+                    break;
+            }
+        };
+    };
+
+    document.onkeydown = function handleKeyDown(e)
+    {
+        var key = e.keyCode;
+        var newDirection;
+        
+        switch(key)
+        {
+            case 37:
+                newDirection = "left";
+                break;
+            case 38:
+                newDirection = "up";
+                break;
+            case 39:
+                newDirection = "right";
+                break;
+            case 40:
+                newDirection = "down";
+                break;
+        }
+    };
 }
+
