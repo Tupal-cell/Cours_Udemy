b393c879aa5a4ff27b0594fab0a5613074ceb956
Tupal-cell <55320896+Tupal-cell@users.noreply.github.com>
2020-05-20 16:35:15 +0200

    apple

--- Snake/script.js
+++ Snake/script.js
@@ -6,6 +6,8 @@ window.onload = function()
     var ctx;
     var delay = 100;
     var snakee;
+    var applee;
+
     init();
 
     function init () 
@@ -17,6 +19,7 @@ window.onload = function()
         document.body.appendChild(canvas);
         ctx = canvas.getContext('2d');
         snakee =new Snake ([[6,4], [5,4], [4,4]], "right");
+        applee =new Apple ([10,10]);
         refreshCanvas();
     };
 
@@ -25,6 +28,7 @@ window.onload = function()
         ctx.clearRect(0,0,canvasWidth, canvasHeight);
         snakee.advance();
         snakee.draw();
+        applee.draw();
         setTimeout(refreshCanvas,delay);
     };
 
@@ -98,6 +102,23 @@ window.onload = function()
             {
                 this.direction = newDirection;
             }
+        };
+    }
+
+    function Apple(position)
+    {
+        this.position = position;
+        this.draw = function()
+        {
+            ctx.save();
+            ctx.fillStyle ="#33cc33";
+            ctx.beginPath();
+            var radius = blockSize/2;
+            var x = position[0]*blockSize + radius;
+            var y = position[1]*blockSize + radius;
+            ctx.arc(x,y, radius, 0, Math.PI*2 , true);
+            ctx.fill();
+            ctx.restore();
         };
     }
 
