7dfac7e69843f58f06fb286ee2b6f99dd0340690
Tupal-cell <55320896+Tupal-cell@users.noreply.github.com>
2020-05-20 18:06:21 +0200

    eat

--- Snake/script.js
+++ Snake/script.js
@@ -7,6 +7,8 @@ window.onload = function()
     var delay = 100;
     var snakee;
     var applee;
+    var widthInBlocks = canvasWidth/blockSize;
+    var heightInBlocks = canvasHeight/blockSize;
 
     init();
 
@@ -25,11 +27,30 @@ window.onload = function()
 
     function refreshCanvas ()
     {
-        ctx.clearRect(0,0,canvasWidth, canvasHeight);
+        
         snakee.advance();
-        snakee.draw();
-        applee.draw();
-        setTimeout(refreshCanvas,delay);
+        if (snakee.checkCollision())
+        {
+            // GAME OVER
+        }
+        else
+        {
+            if(snakee.isEatingApple(applee))
+            {
+                snakee.ateApple = true; 
+                do
+                {
+                    applee.setNewPosition();
+                }
+                while(applee.isOnSnake(snakee))
+                
+            }
+            ctx.clearRect(0,0,canvasWidth, canvasHeight);
+            snakee.draw();
+            applee.draw();
+            setTimeout(refreshCanvas,delay);
+        }
+
     };
 
     function drawBlock(ctx, position,)
@@ -43,6 +64,7 @@ window.onload = function()
     {
         this.body = body;
         this.direction = direction;
+        this.ateApple = false;
 
         this.draw = function () 
         {
@@ -77,7 +99,11 @@ window.onload = function()
             }
 
             this.body.unshift(nextPosition);
+            if (!this.ateApple)
             this.body.pop();
+            else
+                this.ateApple = false;
+                
         };
 
         this.setDirection = function(newDirection)
@@ -103,6 +129,45 @@ window.onload = function()
                 this.direction = newDirection;
             }
         };
+        this.checkCollision = function()
+        {
+            var wallCollision = false;
+            var snakeCollision = false;
+            var head = this.body[0];
+            var rest = this.body.slice(1);
+            var snakeX = head[0];
+            var snakeY = head[1];
+            var minX = 0;
+            var minY = 0;
+            var maxX = widthInBlocks - 1;
+            var maxY = heightInBlocks - 1;
+            var isNotBetweenHorizontalWalls = snakeX < minX || snakeX > maxX;
+            var isNotBetweenVerticalWalls = snakeY < minY || snakeY > maxY;
+
+            if (isNotBetweenHorizontalWalls || isNotBetweenVerticalWalls)
+            {
+                wallCollision = true;
+            }
+
+            for(var i=0; i < rest.length ; i++)
+            {
+                if(snakeX === rest[i][0] && snakeY === rest[i][1])
+                {
+                    snakeCollision = true;
+                }
+            }
+            return wallCollision || snakeCollision;
+ 
+        };
+        this.isEatingApple = function(appleToEat)
+        {
+            var head = this.body[0];
+            if(head[0] === appleToEat.position[0] && head[1] === appleToEat.position[1])
+            {
+                return true;
+            }
+                else false;
+        }
     }
 
     function Apple(position)
@@ -114,11 +179,30 @@ window.onload = function()
             ctx.fillStyle ="#33cc33";
             ctx.beginPath();
             var radius = blockSize/2;
-            var x = position[0]*blockSize + radius;
-            var y = position[1]*blockSize + radius;
+            var x = this.position[0]*blockSize + radius;
+            var y = this.position[1]*blockSize + radius;
             ctx.arc(x,y, radius, 0, Math.PI*2 , true);
             ctx.fill();
             ctx.restore();
+        };
+        this.setNewPosition = function()
+        {
+            var newX = Math.round(Math.random() * (widthInBlocks -1));
+            var newY = Math.round(Math.random() * (heightInBlocks -1));
+            this.position = [newX, newY];
+        };
+        this.isOnSnake = function(snakeToCheck)
+        {
+            var isOnSnake = false;
+
+            for(var i=0 ; i < snakeToCheck.body.length; i++)
+            {
+                if(this.position[0] === snakeToCheck.body[i][0] && this.position[1] === snakeToCheck.body[i][1])
+                {
+                    isOnSnake = true;
+                }
+            }
+            return isOnSnake;
         };
     }
 
