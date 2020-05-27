cc295e056402453f2a83cd40a717f243e2ab48d2
Tupal-cell <chris.quev@gmail.com>
2020-05-27 16:47:32 +0200

    base

---
+++ Jquery/script.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+//     $("#p1").click(function(){
+//         $("#p5").hide();
+//     });
+// });
+
+$(document).ready(function(){
+    $("#p1").mouseenter(function(){
+        $("#p5").hide();
+    });
+});
