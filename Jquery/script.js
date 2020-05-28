1b0eedac85de3c79a8f771073ae6edd875665b98
Tupal-cell <chris.quev@gmail.com>
2020-05-28 17:08:39 +0200

    animate button

--- Jquery/script.js
+++ Jquery/script.js
@@ -4,8 +4,34 @@
 //     });
 // });
 
-$(document).ready(function(){
-    $("#p1").mouseenter(function(){
-        $("#p5").hide();
-    });
-});
+// $(document).ready(function(){
+//     $("#p1").mouseenter(function(){
+//         $("#p5").hide();
+//     });
+// });
+
+// $(function(){
+
+//     $("#p1").click(function(){
+//         $("#p4").hide();
+//     });
+// });
+
+// $(function(){
+
+//     $(selector).animate(arg1, arg2, arg3)
+
+
+// })
+
+$(function(){
+    $("button").click(function(){
+        $("div").animate({
+            left: '600px',
+            opacity: 0.2,
+            width: '200px',
+            height: '200px'}
+            , 3000 )
+
+    })
+})
