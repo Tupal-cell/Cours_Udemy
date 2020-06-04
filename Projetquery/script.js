6894ce4762dbe469ab752b08b540cc8752730217
Tupal-cell <chris.quev@gmail.com>
2020-06-04 21:42:24 +0200

    font

---
+++ Projetquery/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function()
+{
+    var $mainMenuItems = $("#main-menu ul").children("li"),
+        totalMainMenuItems = $mainMenuItems.length,
+        openIndex = -1,
+
+        init = function(){
+            $mainMenuItems.children(".images").click(function(){
+                var newIndex = $(this)
+            });
+        };
+});
