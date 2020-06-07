731f887a48b7429adb76fda141a4920644a38aff
Tupal-cell <55320896+Tupal-cell@users.noreply.github.com>
2020-06-07 16:36:24 +0200

    commit

--- jquery/script.js
+++ jquery/script.js
@@ -1,4 +1,60 @@
-$(document).ready(function(){
+// $(document).ready(function(){
+//     $("#p1").click(function(){
+//         $("#p5").hide();
+//     });
+// });
 
-    $("h1").hide();
+// $(document).ready(function(){
+//     $("#p1").mouseenter(function(){
+//         $("#p5").hide();
+//     });
+// });
+
+// $(function(){
+
+//     $("#p1").click(function(){
+//         $("#p4").hide();
+//     });
+// });
+
+// $(function(){
+
+//     $(selector).animate(arg1, arg2, arg3)
+
+
+// })
+
+// $(function(){
+//     $("button").click(function(){
+//         $("div").animate({
+//             left: '600px',
+//             opacity: 0.2,
+//             width: '200px',
+//             height: '200px'}
+//             , 3000 )
+
+//     })
+// })
+
+// $(function(){
+//     $("#start").click(function(){
+//         // var myVar = $("div").attr("style");
+//         // console.log(myVar);
+//         $("#p1").text("Voila mon nouveau texte");
+
+//     });
+// });
+
+
+$(function(){
+    $("#but1").click(function()
+    {
+        $("#p1,#p2").toggleClass("red");
+    })
+    
+    //     $("#but2").click(function()
+    // {
+    //         $("#p1,#p2").removeClass("red");
+
+    // })
 })
